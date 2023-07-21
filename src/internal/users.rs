16079449575a832922898f6c84ef0726277f6cda
Cassiano Rodrigues - WSL <rodrigues.cassiano2007@gmail.com>
2023-07-21 02:31:35 -0300

    style(internal): change parser variable name

--- src/internal/users.rs
+++ src/internal/users.rs
@@ -5,8 +5,8 @@ use serenity::model::prelude::UserId;
 
 pub const USERS_FILE: &str = "/src/static/users.json";
 
-fn parser_untyped(json_data: String) -> Result<HashMap<String, UserId>, serde_json::Error> {
-    let v: HashMap<String, UserId> = serde_json::from_str(json_data.as_str())?;
+fn parse_users(users_json: String) -> Result<HashMap<String, UserId>, serde_json::Error> {
+    let v: HashMap<String, UserId> = serde_json::from_str(users_json.as_str())?;
     Ok(v)
 }
 
@@ -14,7 +14,7 @@ fn get_users() -> HashMap<String, UserId> {
     let users = fs::read_to_string(format!("{}{}", env!("CARGO_MANIFEST_DIR"), USERS_FILE))
         .expect("Something went wrong reading the file");
 
-    let users = parser_untyped(users);
+    let users = parse_users(users);
 
     users.unwrap()
 }
