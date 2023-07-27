89ffcfffc9e8965f0f0db1ba85532f194e1cea82
Kszinhu@POP_os <rodrigues.cassiano2007@gmail.com>
2023-07-27 01:39:17 -0300

    feat(integration): add jukera integration

---
+++ src/integrations/jukera.rs
@@ -0,0 +1,39 @@
+use super::{CallbackFn, Integration, IntegrationType};
+use crate::internal::users::USERS;
+
+use serenity::async_trait;
+use serenity::model::{channel::Message, gateway::Activity, id::UserId};
+use serenity::prelude::Context;
+
+struct Jukera {}
+
+#[async_trait]
+impl CallbackFn for Jukera {
+    async fn run(&self, message: &Message, ctx: &Context, user_id: &UserId) {
+        run(message, ctx, user_id).await;
+    }
+}
+
+async fn run(message: &Message, ctx: &Context, user_id: &UserId) {
+    if user_id == USERS.get("jukes_box").unwrap() {
+        let current_music = message
+            .embeds
+            .first()
+            .unwrap()
+            .description
+            .as_ref()
+            .unwrap();
+
+        ctx.set_activity(Activity::listening(current_music)).await;
+    }
+    println!("{:?}", message);
+}
+
+pub fn register() -> Integration {
+    Integration::new(
+        "jukera".to_string(),
+        "Jukera Integration, Listening to jukes_box".to_string(),
+        IntegrationType::Chat,
+        Box::new(Jukera {}),
+    )
+}
