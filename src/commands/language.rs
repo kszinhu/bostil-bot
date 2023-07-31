5b311ffb5952aff389f27cb161d0bd039b04c953
Kszinhu@POP_os <rodrigues.cassiano2007@gmail.com>
2023-07-31 02:43:04 -0300

    feat: show current language and improve translations

--- src/commands/language.rs
+++ src/commands/language.rs
@@ -1,5 +1,5 @@
 use crate::database::locale::apply_locale;
-use rust_i18n::t;
+use rust_i18n::{locale as current_locale, t};
 
 use serenity::builder::CreateApplicationCommand;
 use serenity::client::Context;
@@ -8,26 +8,35 @@ use serenity::model::prelude::{
 };
 
 pub async fn run(options: &Vec<CommandDataOption>, _ctx: &Context, guild_id: &GuildId) -> String {
-    let language = options[0].value.as_ref().unwrap().as_str().unwrap();
+    if let Some(language_option) = options.get(0) {
+        let selected_language = language_option.value.as_ref().unwrap().as_str().unwrap();
 
-    apply_locale(language, &guild_id, false);
+        apply_locale(selected_language, &guild_id, false);
 
-    t!("commands.language.reply", "language_code" => t!(&format!("commands.language.{}", language)))
+        let current_locale_name = t!(&format!(
+            "commands.language.{}",
+            language_option.value.as_ref().unwrap()
+        ));
+        t!("commands.language.reply", "language_name" => current_locale_name)
+    } else {
+        let current_locale_name = t!(&format!("commands.language.{}", current_locale()));
+        t!("commands.language.current_language", "language_code" => current_locale(), "language_name" => current_locale_name)
+    }
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
     command
         .name("language")
         .name_localized("pt-BR", "idioma")
-        .description("Change the bot language")
-        .description_localized("pt-BR", "Altere o idioma do bot")
+        .description("Language Preferences Menu")
+        .description_localized("pt-BR", "Menu de preferências de idioma")
         .create_option(|option| {
             option
-                .name("language")
-                .description("The language to change to")
-                .description_localized("pt-BR", "O idioma irá mudar para")
+                .name("choose_language")
+                .name_localized("pt-BR", "alterar_idioma")
+                .description("Choose the language of preference")
+                .description_localized("pt-BR", "Escolha o idioma de preferência")
                 .kind(command::CommandOptionType::String)
-                .required(true)
                 .add_string_choice_localized(
                     "Portuguese",
                     "pt-BR",
